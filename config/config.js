3f3126bc1294696b622aebcea32903a78bde7353
Chelovekdenis <33042434+Chelovekdenis@users.noreply.github.com>
2020-09-22 14:12:11 +0300

    Delete config.js

--- config/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    // dbInfo: ["usersdb2", "root", "88888888"]
-    database: "usersdb2",
-    username: "root",
-    password: "88888888",
-    sessionSecret: 'W$q12=x5*8%av=}BV'
-}
